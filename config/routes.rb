2392d3b2122a5c655844b07e056113b384f436ae
Elis Memic <elismemic5@gmail.com>
2024-05-15 16:02:24 +0200

    Create simple navbar menu with icons

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   get 'landing_page/index'
   root "landing_page#index"
+  scope controller: :main do
+    get :houses
+    get :favourites
+    get :add_house
+    get :delete_house
+  end
 end
