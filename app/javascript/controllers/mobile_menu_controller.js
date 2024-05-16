02460433012616685ecb67feca1f7aea925e1c66
Elis Memic <elismemic5@gmail.com>
2024-05-16 10:48:39 +0200

    Add x to mobile menu navbar

--- app/javascript/controllers/mobile_menu_controller.js
+++ app/javascript/controllers/mobile_menu_controller.js
@@ -1,9 +1,10 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["menu"];
+  static targets = ["menu", "menuIcon"];
 
   toggleMenu(event) {
-    this.menuTarget.classList.toggle("hidden");
+    this.menuTarget.classList.toggle("-translate-x-full");
+    this.menuIconTargets.forEach((icon) => icon.classList.toggle("hidden"));
   }
 }
