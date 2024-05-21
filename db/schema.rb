2bae7064ca04120ee3f46fe066a07da8f3f580ca
Elis Memic <elismemic5@gmail.com>
2024-05-21 13:53:31 +0200

    update schema to add favourites attribute

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_16_101733) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_20_113406) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -49,6 +49,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_16_101733) do
     t.boolean "availability"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "favourite"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
